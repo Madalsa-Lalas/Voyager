67bd8cbd517c24957382b7eb1109b6dbae9c35b9
maddy.lalas@xplortechnologies.com <maddy.lalas@xplortechnologies.com>
2023-01-18 18:56:54 +1300

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEzNDY5Nw/spectral.js?branch_slug=main&token=a14cb7fb-2b66-4e80-9df9-95c3ce5125e9";
+export default { extends: ruleset };
